650431c40410d1fb1103c9a20382dde75ccce731
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-06-29 10:34:02 -0600

    all sorting is working

--- src/components/AllBlogs.js
+++ src/components/AllBlogs.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 const AllBlogs = ({ blogPosts }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortOrder = searchParams.get("sortOrder") || "asc";
+  const sortField = searchParams.get("sortField") || "createdAt";
+  const blogPostsCopy = blogPosts;
+  const compare = (a, b) => {
+    if (sortOrder.toLowerCase() === "asc") {
+      if (a[sortField] < b[sortField]) {
+        return -1;
+      }
+      if (a[sortField] > b[sortField]) {
+        return 1;
+      }
+
+      return 0;
+    }
+    if (sortOrder.toLowerCase() === "desc") {
+      if (a[sortField] < b[sortField]) {
+        return 1;
+      }
+      if (a[sortField] > b[sortField]) {
+        return -1;
+      }
+      return 0;
+    }
+  };
+  const sortedBlogPosts = blogPostsCopy.sort(compare);
   return (
     <>
       <h1>All Blogs</h1>
       <ul>
-        {blogPosts.map((blog, index) => {
+        {sortedBlogPosts.map((blog, index) => {
           return (
             <li key={index}>
               <p>Id: {blog.id}</p>
@@ -20,5 +47,36 @@ const AllBlogs = ({ blogPosts }) => {
     </>
   );
 };
+
+// const sortBlogs = (sortOrder, sortField, posts) => {
+//   if (sortOrder.toLowerCase() === "asc") {
+//     return posts.sort((a, b) => {
+//       if (sortField === "createdAt") {
+//         return new Date(a[sortField]) - new Date(b[sortField]);
+//       }
+//       if (sortField === "id") {
+//         return Number(a[sortField]) - Number(b[sortField]);
+//       }
+//       return b[sortField] < a[sortField]
+//         ? -1
+//         : b[sortField] > a[sortField]
+//         ? 1
+//         : 0;
+//     });
+//   }
+//   if (sortOrder.toLowerCase() === "desc") {
+//     return posts.sort((a, b) => {
+//       if (sortField === "createdAt") {
+//         return new Date(b[sortField]) - new Date(a[sortField]);
+//       }
+//       return a[sortField] < b[sortField]
+//         ? -1
+//         : a[sortField] > b[sortField]
+//         ? 1
+//         : 0;
+//     });
+//   }
+//   return posts;
+// };
 
 export default AllBlogs;
